403a705d12940dbda8839e9df66b6a5b29e22e5d
Jino Devasia <zynthexion@gmail.com>
2025-09-30 05:34:11 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/components/doctors/add-doctor-form.tsx
+++ src/components/doctors/add-doctor-form.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useForm } from "react";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
@@ -447,5 +447,7 @@ export function AddDoctorForm({ onSave, isOpen, setIsOpen, doctor, departments }
     </Dialog>
   );
 }
+
+    
 
     
