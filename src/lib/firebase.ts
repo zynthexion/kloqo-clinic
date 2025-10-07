ab623c5f41a96bb1bbd7fe819cdc8d55402afe44
Jino Devasia <zynthexion@gmail.com>
2025-10-07 10:08:03 +0000

    {       "type": "service_account",       "project_id": "kloqo-clinic-mul

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyChS5xJikU5j_Grtf9p8p2T-r3f3zQY87c",
-    authDomain: "kloqo-a01a4.firebaseapp.com",
-    projectId: "kloqo-a01a4",
-    storageBucket: "kloqo-a01a4.appspot.com",
-    messagingSenderId: "542385449793",
-    appId: "1:542385449793:web:355d40906a5910b271d492",
+  apiKey: "AIzaSyChS5xJikU5j_Grtf9p8p2T-r3f3zQY87c",
+  authDomain: "kloqo-a01a4.firebaseapp.com",
+  projectId: "kloqo-a01a4",
+  storageBucket: "kloqo-a01a4.appspot.com",
+  messagingSenderId: "542385449793",
+  appId: "1:542385449793:web:355d40906a5910b271d492",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
