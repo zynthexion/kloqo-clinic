be9ccef2e6e56ee80bf183dc939059a230d243bc
Jino Devasia <zynthexion@gmail.com>
2025-10-07 17:09:40 +0000

    Runtime Error
    Error: Invalid src prop (https://plus.unsplash.com/premiu

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
