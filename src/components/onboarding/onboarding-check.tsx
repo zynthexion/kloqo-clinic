38bb20bc41a48d65fba9430d3be251d0f1826625
Jino Devasia <zynthexion@gmail.com>
2025-10-07 10:51:04 +0000

    if clinic doesnt have a nested collection of doctor and department . the

--- src/components/onboarding/onboarding-check.tsx
+++ src/components/onboarding/onboarding-check.tsx
@@ -23,14 +23,13 @@ export function OnboardingCheck() {
         if (userDoc.exists()) {
           const clinicId = userDoc.data()?.clinicId;
           if (!clinicId) {
-            // This case might happen if user doc is created but clinic creation fails
             if (pathname !== '/onboarding') router.push('/onboarding');
             return;
           }
 
-          const departmentsQuery = query(collection(db, 'departments'), where('clinicId', '==', clinicId));
-          const doctorsQuery = query(collection(db, 'doctors'), where('clinicId', '==', clinicId));
-
+          const departmentsQuery = query(collection(db, `clinics/${clinicId}/departments`));
+          const doctorsQuery = query(collection(db, `clinics/${clinicId}/doctors`));
+          
           const [departmentsSnapshot, doctorsSnapshot] = await Promise.all([
             getDocs(departmentsQuery),
             getDocs(doctorsQuery),
@@ -42,7 +41,6 @@ export function OnboardingCheck() {
             router.push('/onboarding');
           }
         } else {
-            // If the user document doesn't exist for some reason, they need to onboard.
             if (pathname !== '/onboarding') router.push('/onboarding');
         }
       } catch (error) {
@@ -50,7 +48,6 @@ export function OnboardingCheck() {
       }
     };
 
-    // A small delay to ensure auth state is fully propagated
     const timer = setTimeout(() => {
         checkOnboardingStatus();
     }, 500);
