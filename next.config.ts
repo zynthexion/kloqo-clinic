ac3756a4757f9525a698938fe54867948971dcbd
Jino Devasia <zynthexion@gmail.com>
2025-10-16 15:42:43 +0000

    const masterDepartments = [   {     id: 'dept-01',     name: 'General Me

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
