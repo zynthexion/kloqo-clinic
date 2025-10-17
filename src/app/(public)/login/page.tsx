649fb9c6697f579f7cbe7a3ab70a808a9ae55f1f
Jino Devasia <zynthexion@gmail.com>
2025-10-17 06:09:01 +0000

    remove login with google button

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -122,9 +122,6 @@ export default function LoginPage() {
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button variant="outline" className="w-full" disabled>
-              Login with Google
-            </Button>
           </form>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{' '}
