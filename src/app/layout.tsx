2262b8c4bd47db24cefbf52fd39082b84802197b
Jino Devasia <zynthexion@gmail.com>
2025-10-18 09:33:00 +0000

    https://firebasestorage.googleapis.com/v0/b/kloqo-clinic-multi-33968-4c5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="h-full bg-background">
       <head>
+        <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/kloqo-clinic-multi-33968-4c50b.firebasestorage.app/o/kloqo_Logo.png?alt=media&token=e31146d5-17c2-4f88-82f0-f5e13fbd3641" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
