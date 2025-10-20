77d1f72791b9393360686a9749844482f81992b2
Jino Devasia <zynthexion@gmail.com>
2025-10-20 16:04:15 +0000

    Try fixing this error: `Runtime Error: Error: useTransition is not defin

--- src/app/(app)/appointments/page.tsx
+++ src/app/(app)/appointments/page.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import { useEffect, useState, useMemo, useRef, useCallback } from "react";
+import { useEffect, useState, useMemo, useRef, useCallback, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
